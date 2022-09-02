0a0dabe97bca6b7a60ceab0d505bb0ba37bcefd2
Khajikhon <olimovkhajikhon@gmail.com>
2022-09-02 17:50:24 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavScrollExample from './components/NavScrollExample';
+
+function App() {
+  return (
+    <div className="App">
+      <NavScrollExample/>
+    </div>
+  );
+}
+
+export default App;
